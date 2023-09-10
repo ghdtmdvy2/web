28514e939a752831baee1c5c6632af5a67de9eea
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2023-09-10 11:42:43 +0900

    git init

---
+++ src/main/java/com/deliver_meeting/DeliverMeetingApplication.java
@@ -0,0 +1,13 @@
+package com.deliver_meeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverMeetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverMeetingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deliver_meeting/DeliverMeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliver_meeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverMeetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
